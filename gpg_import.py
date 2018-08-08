ab705de98d0687b4565a521d6dc86097bcbc1d2e
Brandon Kalinowski <brandon@brandonkalinowski.com>
2018-08-07 21:13:16 -0700

    Implement keybase

--- gpg_import.py
+++ gpg_import.py
@@ -1,118 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# © Thelonius Kort - Feel free to redistribute this with any MIT, GPL, or Apache License
+# © Brandon Kalinowski
+# © Original Code by Thelonius Kort - MIT License
 
-from time import sleep
+# from time import sleep
 import re
 import string
-
-DOCUMENTATION = '''
----
-module: gpg_import
-version_added: 1.9
-short_description:  Manages import and removal of GPG-keys
-description:
-     - Imports, refreshes, and deletes GnuPG keys. When importing or refreshing a key it retries on
-       failure and optionally retries with alternating hostnames to fudge on DNS-caches that prevent
-       round-robin keyservers.
-options:
-  key_id:
-    description:
-      - When attempting to fetch and import, the id of the key requested. When deleting a pre-existing 
-        key, this must be the fingerprint of the key to delete. Either key_file or key_id is required.
-    required: false
-    default: null
-
-  key_file:
-    description:
-      - Filename of key to be imported. Must be on remote machine, not local. Either key_file or key_id is required.
-        Can also be used with delete, module will extract the fingerprint from the provided key file and
-        delete the matching key from the key-chain.
-    required: false
-    default: null
-
-  key_type:
-    description:
-      - What type of key to import. Only applicable to key_file
-    required: true
-    choices: [ "private", "public" ]
-    default: "private"
-
-  bin_path:
-    description:
-      - "Location of GPG binary"
-    require: false
-    default: /usr/bin/gpg
-
-  state:
-    description:
-      - Whether to import (C(present), C(latest)), or remove (C(absent)) a key. C(refreshed) is an alias for C(latest).
-    required: false
-    choices: [ "present", "latest", "refreshed",  "absent" ]
-    default: "present"
-
-  servers:
-    description:
-      - A list of hostnames (or `hkp://`/`hkps://` urls) to try
-    required: false
-    default: "['keys.gnupg.net']"
-
-  tries:
-    description:
-      - How often to try per server
-    required: false
-    default: 3
-    aliases: []
-
-  delay:
-    description:
-      - Delay between retries
-    required: false
-    default: 0.5
-
-  gpg_timeout:
-    description:
-      - Timeout parameter for gpg
-    required: false
-    default: 5
-
-notes: []
-requirements: [ gpg ]
-author: Thelonius Kort
-'''
-
-EXAMPLES = '''
-- name: Install GPG key
-  gpg_import: key_id="0x3804BB82D39DC0E3" state=present
-
-- name: Install GPG key
-  gpg_import:
-    key_id: "0x3804BB82D39DC0E3"
-    bin_path: '/usr/local/bin/gpg'
-
-- name: Install or update GPG key
-  gpg_import:
-    key_id: "0x3804BB82D39DC0E3"
-    state: latest
-    servers:
-      - 'hkp://no.way.ever'
-      - 'keys.gnupg.net'
-      - 'hkps://hkps.pool.sks-keyservers.net'
-
-- name: Install or fail with fake and not fake GPG keys
-  gpg_import:
-    key_id: "{{ item }}"
-    tries: 2
-  with_items:
-    - "0x3804BB82D39DC0E3"
-    - "0x3804BB82D39DC0E4" # fake key fails
-
-- name: import a file-based public key
-  gpg_import: key_type=public state=present key_file=/etc/customer-key/customer.pubkey
-
-- name: import a file-based private key
-  gpg_import: key_type=private state=present key_file=/etc/customer-key/customer.privatekey
-'''
+from ansible.module_utils.basic import *
+from ansible.module_utils.urls import fetch_url
 
 class SafeDict(dict):
     def __missing__(self, key):
@@ -121,7 +16,7 @@ class SafeDict(dict):
 # http://stackoverflow.com/a/33621609/659298
 class SafeFormatter(string.Formatter):
     def __init__(self, default='{{{0}}}'):
-        self.default=default
+        self.default = default
 
     def get_value(self, key, args, kwds):
         if isinstance(key, str):
@@ -141,49 +36,78 @@ class GpgImport(object):
         # named 'debuglist' to avoid 'self.debug()' attempting to work.
         self.debuglist.append(msg)
 
+    def get_keybase(self):
+        url = 'https://keybase.io/' + self.m.params["keybase_user"] + '/pgp_keys.asc'
+        rsp, info = fetch_url(self.m, url=url, timeout=10, method='GET')
+
+        status_code = info["status"]
+        # Check for errors first
+        remote_key = rsp.read()
+        if status_code >= 400:
+            remote_key = info['body']
+
+        # Exceptions in fetch_url may result in a status -1, ensure error in all cases.
+        elif info['status'] == -1:
+            self.m.fail_json(msg=info['msg'], url=url)
+
+        elif info['status'] != 200:
+            self.m.fail_json(
+                msg="Request failed",
+                status_code=info['status'],
+                response=info['msg'], url=url
+            )
+        else:
+            # raw_res = self.m.run_command('/usr/bin/gpg --import', data=remote_key)
+            return remote_key
+
     def _execute_task(self):
         key_present = False
-        if self.key_id:
+
+        if self.keybase_user:
+            self._debug("Keybase user is defined")
+            # self.get_keybase()
             res = self._execute_command('check')
             key_present = res['rc'] == 0
-        elif self.key_type == 'public':
-            filekey = self._get_key_from_file()
-            if filekey:
-                # rerun the original setup with this key in the commands
-                self._setup_creds(filekey)
-                res = self._execute_command('check-public')
-                self._debug('checkpublic: %s' % (str(res)))
+            self.changed = False
+        else:
+            if self.key_id:
+                res = self._execute_command('check')
                 key_present = res['rc'] == 0
-        elif self.key_type == 'private':
-            filekey = self._get_key_from_file()
-            if filekey:
-                # rerun the original setup with this key in the commands
-                self._setup_creds(filekey)
-                res = self._execute_command('check-private')
-                self._debug('checkprivate: %s' % (str(res)))
-                key_present = res['rc'] == 0
+            elif self.key_type == 'public':
+                filekey = self._get_key_from_file()
+                if filekey:
+                    # rerun the original setup with this key in the commands
+                    self._setup_creds(filekey)
+                    res = self._execute_command('check-public')
+                    self._debug('checkpublic: %s' % (str(res)))
+                    key_present = res['rc'] == 0
+            elif self.key_type == 'private':
+                filekey = self._get_key_from_file()
+                if filekey:
+                    # rerun the original setup with this key in the commands
+                    self._setup_creds(filekey)
+                    res = self._execute_command('check-private')
+                    self._debug('checkprivate: %s' % (str(res)))
+                    key_present = res['rc'] == 0
 
         if key_present and self.state == 'absent':
             res = self._execute_command('delete')
             self.changed = res['rc'] == 0
-        elif key_present and self.state in ('latest','refreshed'):
-            res = self._repeat_command('refresh')
+        elif key_present and self.state == 'latest':
+            res = self._execute_command('keybase', data=self.get_keybase())
+            # res = self._repeat_command('refresh')
             self.changed = re.search('gpg:\s+unchanged: 1\n', res['stderr']) is None
-        elif not key_present and self.state in ('present','latest','refreshed'):
-            if self.key_type == 'private':
-                if self.key_file:
-                    res = self._execute_command('import-key')
-                    self._debug('running i-private')
-                else:
-                    self._debug('running recv')
-                    res = self._repeat_command('recv')
+        elif not key_present and self.state in ('present', 'latest'):
+            if self.key_type == 'private' and self.key_file:
+                res = self._execute_command('import-key')
+                self._debug('running i-private')
+            elif self.keybase_user:
+                res = self._execute_command('keybase', data=self.get_keybase())
+
             elif self.key_type == 'public':
                 res = self._execute_command('import-key')
                 self._debug('running i-public')
             self.changed = res['rc'] == 0
-        #elif key_present and self.state == 'xxxxxx':
-        #    res = self._execute_command('xxxxxx')
-        #    self.changed = res['rc'] == 0
         else:
             self.changed = False
             res = {'rc': 0}
@@ -193,18 +117,17 @@ class GpgImport(object):
 
 
     def _setup_creds(self, key_override=None):
-        for k,v in self.m.params.items():
+        for k, v in self.m.params.items():
             setattr(self, k, v)
         if key_override:
             self.key_id = key_override
         self.commands = {
             'check':   '{bin_path} {check_mode} --list-keys {key_id}',
             'delete':  '{bin_path} {check_mode} --batch --yes --delete-secret-and-public-keys {key_id}',
-            'refresh': '{bin_path} {check_mode} --keyserver {url} --keyserver-options timeout={timeout} --refresh-keys {key_id}',
             'check-private':  '{bin_path} {check_mode} --list-secret-keys {key_id}',
-            'recv':    '{bin_path} {check_mode} --keyserver {url} --keyserver-options timeout={timeout} --recv-keys {key_id}',
             'check-public':  '{bin_path} {check_mode} --list-public-keys {key_id}',
-            'import-key': '{bin_path} {check_mode} --batch --import {key_file}'
+            'import-key': '{bin_path} {check_mode} --batch --import {key_file}',
+            'keybase': '{bin_path} {check_mode} --batch --import',
         }
         command_data = {
             'check_mode': '--dry-run' if self.m.check_mode else '',
@@ -213,42 +136,45 @@ class GpgImport(object):
             'key_file': self.key_file
         }
         # sort of a brilliant way of late-binding/double-formatting given here: http://stackoverflow.com/a/17215533/659298
-        for c,l in self.commands.items():
+        for c, l in self.commands.items():
             sf = SafeFormatter()
             self.commands[c] = sf.format(l, **command_data)
-        self.urls = [s if re.match('hkps?://', s)
-                       else 'hkp://%s' % s
-                     for s in self.servers]
+        # self.urls = [s if re.match('hkps?://', s)
+        #                else 'hkp://%s' % s
+        #              for s in self.servers]
         self._debug('set up commands: %s' % (str(self.commands)))
 
-    def _repeat_command(self, cmd):
-        for n in range(self.tries):
-            for u in self.urls:
-                sf = SafeFormatter()
-                full_command = sf.format(
-                    self.commands[cmd], timeout=self.gpg_timeout, url=u
-                )
-                self._debug("full command: %s" % (full_command))
-                raw_res = self.m.run_command(full_command)
-                res = self._legiblify(cmd, raw_res)
-                if res['rc'] == 0:
-                    return res
-                sleep(self.delay)
-        return {'rc': 8888}
+    # def _repeat_command(self, cmd):
+    #     for n in range(self.tries):
+    #         for u in self.urls:
+    #             sf = SafeFormatter()
+    #             full_command = sf.format(
+    #                 self.commands[cmd], timeout=self.gpg_timeout, url=u
+    #             )
+    #             self._debug("full command: %s" % (full_command))
+    #             raw_res = self.m.run_command(full_command)
+    #             res = self._legiblify(cmd, raw_res)
+    #             if res['rc'] == 0:
+    #                 return res
+    #             sleep(self.delay)
+    #     return {'rc': 8888}
 
-    def _execute_command(self, cmd):
+    def _execute_command(self, cmd, data=''):
         self._debug('command: %s' % (str(self.commands[cmd])))
-        raw_res = self.m.run_command(self.commands[cmd])
+        if data:
+            raw_res = self.m.run_command(self.commands[cmd], data=data)
+        else:
+            raw_res = self.m.run_command(self.commands[cmd])
         return self._legiblify(cmd, raw_res)
 
-    def _legiblify(self, sec, res):
+    def _legiblify(self, res):
         """turn tuple to dict and preserve it for debugging"""
         if not hasattr(self, 'log_dic'):
             self.log_dic = {}
-        rdic = dict([k, res[i]] for i,k in enumerate(('rc', 'stdout', 'stderr')))
-        self.log_dic.setdefault(sec, {'tries': [], 'num_tries':  0})
-        self.log_dic[sec]['tries'].append(rdic)
-        self.log_dic[sec]['num_tries'] += 1
+        rdic = dict([k, res[i]] for i, k in enumerate(('rc', 'stdout', 'stderr')))
+        # self.log_dic.setdefault(sec, {'tries': [], 'num_tries':  0})
+        # self.log_dic[sec]['tries'].append(rdic)
+        # self.log_dic[sec]['num_tries'] += 1
         return rdic
 
     def _get_key_from_file(self):
@@ -267,21 +193,20 @@ class GpgImport(object):
             return keysearch.group(1)
         return None
 
+
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
+        argument_spec=dict(
+            keybase_user=dict(type='str'),
             key_id=dict(required=False, type='str'),
+            key_type=dict(default='public', choices=['private', 'public']),
             key_file=dict(required=False, type='str'),
-            servers=dict(default=['keys.gnupg.net'], type='list'),
             bin_path=dict(default='/usr/bin/gpg', type='str'),
-            tries=dict(default=3, type='int'),
-            delay=dict(default=0.5, type='float'),
-            state=dict(default='present', choices=['latest', 'refreshed', 'absent', 'present']),
-            key_type=dict(default='private', choices=['private', 'public']),
-            gpg_timeout=dict(default=5, type='int')
+            state=dict(default='present', choices=['latest', 'absent', 'present']),
         ),
         supports_check_mode=True,
-        required_one_of=[['key_id', 'key_file']]
+        required_one_of=[['keybase_user', 'key_file']],
+        required_together=[['keybase_user', 'key_id']],
     )
 
     gkm = GpgImport(module)
@@ -295,5 +220,4 @@ def main():
     module.exit_json(**result)
 
 
-from ansible.module_utils.basic import *
 main()
